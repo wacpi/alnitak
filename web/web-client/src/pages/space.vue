d9aaa37ce3f7c1cb9b5416a92fb1c68e73fd6c6a
acgkiss <anime@acgkiss.com>
2025-12-27 11:29:12 +0800

    fix: 修复个人空间图片上传bug，增加oss和服务端的https模式！

--- web/web-client/src/pages/space.vue
+++ web/web-client/src/pages/space.vue
@@ -270,6 +270,7 @@ const getUploadVideo = async () => {
     position: absolute;
     top: 12px;
     right: 20px;
+    z-index: 2;
     width: 120px;
     height: 32px;
     color: var(--primary-text-color);
@@ -284,6 +285,9 @@ const getUploadVideo = async () => {
   }
 
   .cover {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
     object-fit: cover;
